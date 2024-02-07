4fbe295dcd4ebc6585f9afb5f377b92481771856
Jules <sonij590833@gmail.com>
2024-02-07 11:54:50 +0100

    fix: on retire timeout car nico ns embete

--- api/gateway/config/dependencies/services_dependencies.php
+++ api/gateway/config/dependencies/services_dependencies.php
@@ -8,16 +8,16 @@ use GuzzleHttp\Client;
 return [
 
     'auth.client' => function (ContainerInterface $c) {
-        return new Client(['base_uri' => 'http://auth_php', 'timeout' => 5.0]);
+        return new Client(['base_uri' => 'http://auth_php']);
     },
 
     'game.client' => function (ContainerInterface $c) {
-        return new Client(['base_uri' => 'http://service_geoquizz_php', 'timeout' => 5.0]);
+        return new Client(['base_uri' => 'http://service_geoquizz_php']);
     },
 
     'localisation.client' => function (ContainerInterface $c) {
         $directus = gethostbyname('directus');
-        return new Client(['base_uri' => 'http://'.$directus.':8055', 'timeout' => 5.0]);
+        return new Client(['base_uri' => 'http://'.$directus.':8055']);
     },
 
 ];
